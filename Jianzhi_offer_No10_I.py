ed5930bf7aa5b9746d6446a319858b592b2626af
yujann <yujann795@qq.com>
2021-07-12 14:58:32 +0800

    剑指offer10-I 斐波那契

---
+++ Jianzhi_offer_No10_I.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        Fibo = [0, 1]
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        for i in range(2, n + 1):
+            Fibo.append(Fibo[0] + Fibo[1])
+            del Fibo[0]
+        return int(Fibo[1] % (1e9+7))
+
+
+s = Solution()
+print(s.fib(81))
