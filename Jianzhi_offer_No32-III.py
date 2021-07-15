1cf4a6fedd64e243619ade2cefbcf7c120adbdf6
yujann <yujann795@qq.com>
2021-07-15 18:27:32 +0800

    剑指 Offer 32 - II. 从上到下打印二叉树 III 双队列保存状态 字节面试原题

---
+++ Jianzhi_offer_No32-III.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        ret = []
+        q1 = [root]
+        q2 = []
+        flag = 0
+        while q1:
+            tmp = []
+            q2 = q1[:]
+            q1.clear()
+            while q2:
+                tmp.append(q2[0].val)
+                if q2[0].left is not None:
+                    q1.append(q2[0].left)
+                if q2[0].right is not None:
+                    q1.append(q2[0].right)
+                del q2[0]
+            if flag:
+                tmp = tmp[::-1]
+            flag = not flag
+            ret.append(tmp)
+        return ret
