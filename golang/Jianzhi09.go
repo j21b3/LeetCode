82d4f30a2dd9794dc934e0cb63259ceb79d38e46
j21b3 <842000795@qq.com>
2022-08-09 15:19:42 +0800

    剑指 Offer 09. 用两个栈实现队列 优化流程

--- golang/Jianzhi09.go
+++ golang/Jianzhi09.go
@@ -21,18 +21,18 @@ func (this *CQueue) AppendTail(value int) {
 }
 
 func (this *CQueue) DeleteHead() int {
-	if len(this.stackOut) > 0 {
-		ret := this.stackOut[len(this.stackOut)-1]
-		this.stackOut = this.stackOut[:len(this.stackOut)-1]
-		return ret
-	} else if len(this.stackIn) <= 0 {
+	//若是in和out都没有数据就返回-1
+	if len(this.stackIn) <= 0 && len(this.stackOut) <= 0 {
 		return -1
 	} else {
-		for idx := len(this.stackIn) - 1; idx >= 0; idx-- {
-			this.stackOut = append(this.stackOut, this.stackIn[idx])
+		//要是out没有数据了就把in的倒到out里
+		if len(this.stackOut) <= 0 {
+			for idx := len(this.stackIn) - 1; idx >= 0; idx-- {
+				this.stackOut = append(this.stackOut, this.stackIn[idx])
+			}
+			this.stackIn = make([]int, 0)
 		}
-		this.stackIn = make([]int, 0)
-
+		//从out里取数据
 		ret := this.stackOut[len(this.stackOut)-1]
 		this.stackOut = this.stackOut[:len(this.stackOut)-1]
 		return ret
