219c6491faf5388e798c180a0ae6586443347aa5
j21b3 <842000795@qq.com>
2022-08-22 16:06:15 +0800

    剑指 Offer 22. 链表中倒数第k个节点

---
+++ golang/Jianzhi22.go
@@ -0,0 +1,28 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getKthFromEnd(head *ListNode, k int) *ListNode {
+	slow, fast := head, head
+
+	for i := 0; i < k; i++ {
+		fast = fast.Next
+	}
+
+	for fast != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+}
